621271af870ab2332d534d7142d66960cd4ebb44
Joan Lucas <reecaptcha@gmail.com>
2023-11-27 23:55:01 -0300

    Added nvim-ufo as dependency.

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,6 +1,7 @@
 return {
 	'nvim-treesitter/nvim-treesitter',
 	build = ':TSUpdate',
+	dependencies = {'kevinhwang91/nvim-ufo'},
 	config = function()
 		require('nvim-treesitter.configs').setup({
 			ensure_installed = {'python', 'lua'},
