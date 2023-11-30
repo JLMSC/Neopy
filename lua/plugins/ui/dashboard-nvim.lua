b848d7b592e4199233d4a531303c2d0b0b216290
Joan Lucas <reecaptcha@gmail.com>
2023-11-29 22:21:14 -0300

    Changed file shortcuts to numbers.

--- lua/plugins/ui/dashboard-nvim.lua
+++ lua/plugins/ui/dashboard-nvim.lua
@@ -5,6 +5,7 @@ return {
 	config = function()
 		require('dashboard').setup({
             theme = 'hyper',
+			shortcut_type = 'number',
             config = {
                 week_header = {
                     enable = true,
